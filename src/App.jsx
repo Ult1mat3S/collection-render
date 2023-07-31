9483af82124fc12d0eb7d608577d2726a6ad7bee
Ult1mat3S <52617311+Ult1mat3S@users.noreply.github.com>
2023-07-31 03:04:34 -0600

    feat: add phonebook, not allow duplicate names

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,49 @@
-import Course from "./components/Course";
-import Header from "./components/Header";
+import { useState } from "react";
 
-const App = ({ course }) => {
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addName = (event) => {
+    event.preventDefault();
+    const nameObject = {
+      name: newName,
+    };
+
+    const isNameInArray =
+      persons.find((el) => {
+        return el.name === newName;
+      }) !== undefined;
+
+    if (isNameInArray === true) {
+      alert(`${newName} is already added to the phonebook`);
+    } else {
+      setPersons(persons.concat(nameObject));
+      setNewName("");
+    }
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
   return (
     <div>
-      <Header course={course} />
-      <Course course={course} />
+      <h2>Phonebook</h2>
+      {/* <div>debug: {newName}</div> */}
+
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,53 +3,26 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-export const course = [
+const notes = [
   {
-    name: "Half Stack application development",
     id: 1,
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: "Redux",
-        exercises: 11,
-        id: 4,
-      },
-    ],
+    content: "HTML is easy",
+    important: true,
   },
   {
-    name: "Node.js",
     id: 2,
-    parts: [
-      {
-        name: "Routing",
-        exercises: 3,
-        id: 1,
-      },
-      {
-        name: "Middlewares",
-        exercises: 7,
-        id: 2,
-      },
-    ],
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
   },
 ];
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App course={course} />
+    <App notes={notes} />
   </React.StrictMode>
 );
