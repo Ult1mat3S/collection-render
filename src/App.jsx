b13e8bc448d1dfa494b52e0eb3175ba053c8a8ef
Ult1mat3S <52617311+Ult1mat3S@users.noreply.github.com>
2023-07-31 03:14:55 -0600

    feat: add number field, rename header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "123-456-7890" }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const addName = (event) => {
     event.preventDefault();
     const nameObject = {
       name: newName,
+      number: newNumber,
     };
 
     const isNameInArray =
@@ -20,11 +22,16 @@ const App = () => {
     } else {
       setPersons(persons.concat(nameObject));
       setNewName("");
+      setNewNumber("");
     }
   };
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
   };
 
   return (
@@ -37,12 +44,17 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          <div>
+            number: <input value={newNumber} onChange={handleNumberChange} />
+          </div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
+      <h2>Contacts</h2>
       {persons.map((person) => (
-        <p key={person.name}>{person.name}</p>
+        <p key={person.name}>
+          {person.name} - {person.number}
+        </p>
       ))}
     </div>
   );
