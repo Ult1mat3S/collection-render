84413dd8ec143977afa0ea2d8bef47e3b439b3dc
Ult1mat3S <52617311+Ult1mat3S@users.noreply.github.com>
2023-08-22 02:57:10 -0600

    feat: add notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,51 @@
-import { useState } from "react";
-import Filter from "./components/Filter";
-import PersonForm from "./components/PersonForm";
-import Persons from "./components/Persons";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Note from "./components/Note";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
-  const [searchFilter, setSearchFilter] = useState("");
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(false);
 
-  const addName = (e) => {
-    e.preventDefault();
-    const nameObject = {
-      name: newName,
-      number: newNumber,
+  useEffect(() => {
+    axios.get("http://localhost:3001/notes").then((response) => {
+      setNotes(response.data);
+    });
+  }, []);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
     };
 
-    const isNameInArray =
-      persons.find((el) => {
-        return el.name === newName;
-      }) !== undefined;
-
-    if (isNameInArray === true) {
-      alert(`${newName} is already added to the phonebook`);
-    } else {
-      setPersons(persons.concat(nameObject));
-      setNewName("");
-      setNewNumber("");
-    }
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
   };
 
-  const handleNameChange = (e) => {
-    setNewName(e.target.value);
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value);
   };
 
-  const handleNumberChange = (e) => {
-    setNewNumber(e.target.value);
-  };
-
-  const handleSearchFilter = (e) => {
-    setSearchFilter(e.target.value);
-  };
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
   return (
     <div>
-      <h2>Phonebook</h2>
-
-      <Filter value={searchFilter} onChange={handleSearchFilter} />
-
-      <h2>Add New Contact</h2>
-      <PersonForm
-        newName={newName}
-        newNumber={newNumber}
-        handleNameChange={handleNameChange}
-        handleNumberChange={handleNumberChange}
-        addName={addName}
-      />
-      <h2>Contacts</h2>
-      <Persons persons={persons} searchFilter={searchFilter} />
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>show {showAll ? "important" : "all"}</button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   );
 };
